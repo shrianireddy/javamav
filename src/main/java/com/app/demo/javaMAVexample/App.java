9c5a22ac0617610201a7b01baa66ec78835a839c
shrianireddy <shrianireddy@gmail.com>
2024-11-23 02:30:38 +0530

    sample maven java

---
+++ src/main/java/com/app/demo/javaMAVexample/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.javaMAVexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Shriani" );
+    }
+}
